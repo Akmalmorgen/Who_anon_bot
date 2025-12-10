82efd5855118364237a0fbcb5a7c8c3bc07df15c
Gojo <148787541+Akmalmorgen@users.noreply.github.com>
2025-12-10 14:45:23 +0500

    Create settings.py

---
+++ Who_anon_bot/who_anon_bot/config/settings.py
@@ -0,0 +1,10 @@
+# who_anon_bot/config/settings.py
+# Настройки бота Who?Anonim™
+
+TOKEN = "8134649307:AAH3k13igSnG9t6lGYYTvu7e2D4RiKaCXMI"
+
+# Список админов (можно много)
+ADMINS = {7967404620}    # замени на свой ID     # добавляй других админов
+
+# Путь к локальной SQLite базе
+DATABASE_PATH = "who_anon_bot/database.sqlite3"
